850bbca58a2ddbee54ee0eb306ad751bd69a1841
Wkana <willian.kana@tecsup.edu.pe>
2024-10-02 16:46:21 -0500

    Initial Commit

---
+++ KanaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KanaColeccionDeJuegos
+//
+//  Created by Willian Kana Choquenaira on 2/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
